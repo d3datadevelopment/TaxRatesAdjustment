2e882701af64f7d970ccca1ca1b7936ea21d1b79
Daniel Seifert <ds@shopmodule.com>
2020-06-05 13:38:40 +0200

    initial

---
+++ Models/raiseTaxRate.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace D3\TaxRatesAdjustment\Model;
+
+class raiseTaxRate
+{
+    public function run()
+    {
+        dumpvar(__METHOD__);
+    }
+}
---
+++ bootstrap.php
@@ -0,0 +1,50 @@
+<?php 
+
+use OxidEsales\ComposerPlugin\Installer\Package\ShopPackageInstaller;
+
+$bootstrapFileName = getenv('ESHOP_BOOTSTRAP_PATH');
+if (!empty($bootstrapFileName)) {
+    $bootstrapFileName = realpath(trim(getenv('ESHOP_BOOTSTRAP_PATH')));
+} else {
+    $count = 0;
+    $bootstrapFileName = '../../'. ShopPackageInstaller::SHOP_SOURCE_DIRECTORY .'/bootstrap.php';
+    $currentDirectory = __DIR__ . '/';
+    while ($count < 5) {
+        $count++;
+        if (file_exists($currentDirectory . $bootstrapFileName)) {
+            $bootstrapFileName = $currentDirectory . $bootstrapFileName;
+            break;
+        }
+        $bootstrapFileName = '../' . $bootstrapFileName;
+    }
+}
+
+if (!(file_exists($bootstrapFileName) && !is_dir($bootstrapFileName))) {
+    $items = [
+        "Unable to find eShop bootstrap.php file.",
+        "You can override the path by using ESHOP_BOOTSTRAP_PATH environment variable.",
+        "\n"
+    ];
+
+    $message = implode(" ", $items);
+
+    die($message);
+}
+
+require_once($bootstrapFileName);
+
+define('OX_IS_ADMIN', true);
+
+if (false == function_exists('isAdmin')) {
+    /**
+     * @return bool
+     */
+    function isAdmin()
+    {
+        if (defined('OX_IS_ADMIN')) {
+            return OX_IS_ADMIN;
+        }
+
+        return true;
+    }
+}
